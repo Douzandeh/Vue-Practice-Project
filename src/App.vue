df35ce36676f4764f8f433bef43ba9beed157f57
Hossein Douzandeh <Douzandeh.k@email.come>
2023-01-21 18:09:29 +0330

    Added file

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{ title }}</h1>
+<Modal :header="header" :text="text" theme = "sale"/>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue"
+
+export default {
+  name: "App",
+  components: {Modal},
+  data() {
+    return {
+      header: "Sign up for a Giveaway",
+      text: "Grab the course for half price"
+    };
+  },
+  methods: {
+    handelEvent(){
+      console.log("event");
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("Ali")
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: solid 1px #ccc;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
